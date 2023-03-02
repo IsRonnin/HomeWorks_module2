f137c30d06aeba1d6485f1bc5150875d668615cf
artem <artemvasenko09@mail.ru>
2023-03-02 19:35:04 +0300

    Пофиксил файл теста.

--- home_work_1/env/Scripts/activate.bat
+++ home_work_1/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\artem\Desktop\Py_tst\2\again\env
+set VIRTUAL_ENV=C:\Users\artem\Desktop\Home_worksM2\2\again\home_work_1\env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- home_work_1/test.py
+++ home_work_1/test.py
@@ -1,6 +1,6 @@
 import os
 
-with open(os.path.dirname(__file__) + "/requirements.txt", encoding="utf-16") as requirements_file:
+with open(os.path.dirname(__file__) + "/requirements.txt", encoding="utf-8") as requirements_file:
   if (os.path.isdir(os.path.dirname(__file__) + "/env/Lib/site-packages/pynput")
   and "pynput==" in requirements_file.read()):
     print("execution t")
